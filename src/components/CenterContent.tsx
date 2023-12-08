afa91f9f2fe41ef0ac33d8cfd05b45e797499d51
Evgeny <evgeny.buturlia@gmail.com>
2023-12-08 15:05:00 +0400

    initial commit

---
+++ src/components/CenterContent.tsx
@@ -0,0 +1,7 @@
+export default function CenterContent() {
+  return (
+    <div className="flex-grow p-4 overflow-auto">
+      {/* Center content here */}
+    </div>
+  );
+}
---
+++ src/components/LeftSidebar.tsx
@@ -0,0 +1,7 @@
+export default function LeftSidebar() {
+  return (
+    <div className="bg-blue-500 w-full md:w-1/4 lg:w-1/5 xl:w-1/4 2xl:w-1/7 p-4 hidden md:block">
+      {/* Sidebar content */}
+    </div>
+  );
+}
