850c0fbe6f760e15705d9ac5335a60e72285cd03
Evgeny <evgeny.buturlia@gmail.com>
2023-12-08 22:36:31 +0400

    fix some styles for sidebars

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -36,7 +36,7 @@ const notes = [
 
 export default function LeftSidebar() {
   return (
-    <div className="bg-blue-500 w-full md:w-1/4 lg:w-1/5 xl:w-1/4 2xl:w-1/7 p-4 hidden md:block">
+    <div className="w-full md:w-1/4 lg:w-1/5 xl:w-1/4 2xl:w-1/7 p-4 hidden md:block">
       <Section title="Conversations">
         {conversations.map((conversation) => (
           <ConversationItem
--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -45,7 +45,7 @@ const collections = [
 
 export default function RightSidebar() {
   return (
-    <div className="bg-green-500 w-full md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 p-4 hidden lg:block">
+    <div className="w-full md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 p-4 hidden lg:block">
       <Section title="Collections" header={<Search />}>
         {collections.map((collection) => (
           <CollectionItem
