9fc3f280d63875440adf2f8bf948cf5024933517
Evgeny <evgeny.buturlia@gmail.com>
2023-12-11 21:23:47 +0400

    image added logo

--- src/components/CenterContent.tsx
+++ src/components/CenterContent.tsx
@@ -1,10 +1,17 @@
 import SessionProviderWrapper from "@/utils/SessionProviderWrapper";
 import AuthStatus from "./AuthStatus";
+import Image from "next/image";
 
 export default async function CenterContent() {
   return (
     <div className="flex justify-center items-center flex-grow flex-col p-4 overflow-auto ">
-      <p className="text-black">Ryght</p>
+      <Image
+        className="my-10"
+        src="https://registry-dev.syntheticabio.net/assets/images/ry_logo.svg"
+        alt="Description"
+        width="200"
+        height="100"
+      />
       <SessionProviderWrapper>
         <AuthStatus />
       </SessionProviderWrapper>
