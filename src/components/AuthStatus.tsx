3593c3bef930e93c378221b5608c58ce4a1ed481
Evgeny <evgeny.buturlia@gmail.com>
2023-12-11 18:03:30 +0400

    refactor AuthStatus component

--- src/components/AuthStatus.tsx
+++ src/components/AuthStatus.tsx
@@ -19,47 +19,56 @@ async function keycloakSessionSignin() {
   }
 }
 
-export default function AuthStatus() {
-  const { data: session, status } = useSession();
-
+function useHandleAuthErrors(session, status) {
   useEffect(() => {
-    if (
-      status != "loading" &&
-      session &&
-      session?.error === "RefreshAccessTokenError"
-    ) {
+    if (status !== "loading" && session?.error === "RefreshAccessTokenError") {
       signOut({ callbackUrl: "/" });
     }
   }, [session, status]);
+}
 
-  if (status == "loading") {
+function AuthActionButton({ session }) {
+  const handleLogout = async () => {
+    await keycloakSessionLogOut();
+    signOut({ callbackUrl: "/" });
+  };
+
+  const handleLogin = async () => {
+    await keycloakSessionSignin();
+    signIn();
+  };
+
+  return session ? (
+    <button
+      className="bg-black font-bold text-white py-1 px-2 rounded border border-gray-50"
+      onClick={handleLogout}
+    >
+      Log out
+    </button>
+  ) : (
+    <button
+      className="bg-black font-bold text-white py-1 px-2 rounded border border-gray-50"
+      onClick={handleLogin}
+    >
+      Log in
+    </button>
+  );
+}
+
+export default function AuthStatus() {
+  const { data: session, status } = useSession();
+
+  useHandleAuthErrors(session, status);
+
+  if (status === "loading") {
     return <div className="еext-black">Loading...</div>;
-  } else if (session) {
-    return (
-      <div className="flex flex-col">
-        <span className="text-black">Logged in as</span>
-        <span className="text-black">{session?.user?.email}</span>
-        <button
-          className="bg-black font-bold text-white py-1 px-2 rounded border border-gray-50"
-          onClick={() => {
-            keycloakSessionLogOut().then(() => signOut({ callbackUrl: "/" }));
-          }}
-        >
-          Log out
-        </button>
-      </div>
-    );
   }
 
   return (
     <div className="flex flex-col">
-      <span className="text-black">Not logged in.</span>
-      <button
-        className="bg-black font-bold text-white py-1 px-2 rounded border border-gray-50"
-        onClick={() => keycloakSessionSignin().then(() => signIn())}
-      >
-        Log in
-      </button>
+      <span className="text-black">{session ? "Logged in as" : "Not logged in."}</span>
+      {session && <span className="text-black">{session.user.email}</span>}
+      <AuthActionButton session={session} />
     </div>
   );
 }
