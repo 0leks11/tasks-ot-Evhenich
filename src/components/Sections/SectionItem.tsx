15342fee723a0d66c7183b2dad6aa09f288e2aec
Evgeny <evgeny.buturlia@gmail.com>
2023-12-11 21:45:42 +0400

    fix truncate in SectionItem

--- src/components/Sections/SectionItem.tsx
+++ src/components/Sections/SectionItem.tsx
@@ -9,7 +9,7 @@ const SectionItem = ({ title, children }: SectionItemProps) => {
   return (
     <div className="group flex flex-row items-center md:pl-4 hover:bg-violet-100 py-2 cursor-pointer">
       {children}
-      <span className="text-black truncate max-w-[calc(100%-90px)] w-full relative xl:left-6 lg:left-4 md:left-3 left-2 group-hover:font-medium">
+      <span className="text-black truncate max-w-[calc(100%-50px)] w-full relative xl:left-6 lg:left-4 md:left-3 left-2 group-hover:font-medium">
         {title}
       </span>
     </div>
