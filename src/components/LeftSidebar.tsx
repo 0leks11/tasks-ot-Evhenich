5cada7f7b8284cfe5fefa022d0931f0daec87bb5
Evgeny <evgeny.buturlia@gmail.com>
2023-12-11 12:50:48 +0400

    conv request and data render implemented

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -5,25 +5,7 @@ import ConversationItem from "./Sections/ConversationItem";
 import NotesItem from "./Sections/NotesItem";
 import Search from "./Sections/Search";
 import Section from "./Sections/Section";
-import { useEffect } from "react";
-
-const conversations = [
-  { id: 1, title: "What is NIH" },
-  { id: 2, title: "How to Bake a Cake" },
-  { id: 3, title: "Introduction to JavaScript" },
-  { id: 4, title: "Exploring National Parks" },
-  { id: 5, title: "The Art of Photography" },
-  { id: 6, title: "History of Space Exploration" },
-  { id: 7, title: "Healthy Eating Habits" },
-  { id: 8, title: "Traveling on a Budget" },
-  { id: 9, title: "Beginner's Guide to Yoga" },
-  { id: 10, title: "Famous Works of Literature" },
-  { id: 11, title: "Mastering Chess Strategies" },
-  { id: 12, title: "DIY Home Improvement Projects" },
-  { id: 13, title: "The World of Birds" },
-  { id: 14, title: "Learning a New Language" },
-  { id: 15, title: "Financial Planning 101" },
-];
+import { useEffect, useState } from "react";
 
 const notes = [
   { id: 1, title: "A couple of words" },
@@ -37,18 +19,36 @@ const notes = [
   { id: 9, title: "Demo Text" },
   { id: 10, title: "Placeholder Title" },
 ];
+type Conversations = {
+  id: string;
+  name: string;
+  updatedAt: string;
+};
 
 export default function LeftSidebar() {
+  const [conversations, setConversations] = useState<Conversations[]>([]);
+
   useEffect(() => {
-    const res = fetch("api/collections", {
-      credentials: "include",
-    });
+    const fetchConversations = async () => {
+      const res = await fetch("api/conversations", {
+        credentials: "include",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        setConversations(data.data.content);
+      } else {
+        console.log("Error with fetchConversations");
+      }
+    };
+    fetchConversations();
   }, []);
+
   return (
     <div className="w-full md:w-1/4 lg:w-1/5 xl:w-1/4 2xl:w-1/7 p-4 hidden md:block">
       <Section title="Conversations">
         {conversations.map((conversation) => (
-          <ConversationItem key={conversation.id} title={conversation.title} />
+          <ConversationItem key={conversation.id} title={conversation.name} />
         ))}
       </Section>
       <Section title="Notes" header={<Search />}>
