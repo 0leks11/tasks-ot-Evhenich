4d37dca835b5404be8d08d895129557013fc9b27
Evgeny <evgeny.buturlia@gmail.com>
2023-12-11 21:11:50 +0400

    fix loading position

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,8 +1,13 @@
 import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
+import { FC } from "react";
 
-export const renderSectionContent = (isLoading, items, ItemComponent) => {
+export const renderSectionContent = (
+  isLoading: boolean,
+  items: any[],
+  ItemComponent: FC<any>
+) => {
   if (isLoading) {
-    return <LoadingSpinner />;
+    return <div className="flex items-center justify-center"><LoadingSpinner /></div>;
   }
 
   return items.map((item) => <ItemComponent key={item.id} title={item.name} />);
